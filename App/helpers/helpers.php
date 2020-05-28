216b2572d08dfb5b3649bc93a879430e301555f3
evaleries <didaarda15@gmail.com>
2020-05-29 04:01:14 +0700

    Move middleware functions

--- App/helpers/helpers.php
+++ App/helpers/helpers.php
@@ -140,31 +140,6 @@ function auth() {
     return session()->get('__auth', null);
 }
 
-function isAuthenticated() {
-    return session()->has('__auth');
-}
-
-/**
- * Middleware function.
- * @return string
- */
-function requireAdmin() {
-    return requireLogin() && auth()->isAdmin() ? null : \App\Core\Route::error(403, 'You don\'t have a permission to access this feature');
-}
-
-/**
- * Middleware function.
- * @param null $to
- * @return void|null
- */
-function requireLogin($to = null) {
-    return ! isAuthenticated() ? \App\Core\Route::redirect('auth/login') : null;
-}
-
-function redirectIfLoggedIn() {
-    return isAuthenticated() ? \App\Core\Route::back() : null;
-}
-
 function old($key, $default = null) {
     return request()->getOldRequest($key, $default);
 }
