ee21fb62945c1b1475a95f3fca55258340d77be7
evaleries <didaarda15@gmail.com>
2020-05-31 10:25:03 +0700

    [WIP] Admin Manage Product

--- App/routes.php
+++ App/routes.php
@@ -1,36 +1,57 @@
 <?php
 
 use App\Core\Route;
+use App\Controllers\Front\CartController;
+use App\Controllers\Front\CheckoutController;
+use App\Controllers\Auth\AuthController;
 
 Route::get('/', '\App\Controllers\Front\HomeController@index');
 Route::get('/categories', '\App\Controllers\Front\HomeController@categories');
 Route::get('/categories/(\w+)', [App\Controllers\Front\HomeController::class, 'category']);
 
-Route::get('/cart', [App\Controllers\Front\CartController::class, 'index']);
-Route::get('/cart/clear', [App\Controllers\Front\CartController::class, 'clear']);
-Route::post('/cart', [App\Controllers\Front\CartController::class, 'add']);
-Route::post('/cart/delete', [App\Controllers\Front\CartController::class, 'delete']);
-Route::post('/cart/update', [App\Controllers\Front\CartController::class, 'update']);
+Route::get('/cart', [CartController::class, 'index']);
+Route::get('/cart/clear', [CartController::class, 'clear']);
+Route::post('/cart', [CartController::class, 'add']);
+Route::post('/cart/delete', [CartController::class, 'delete']);
+Route::post('/cart/update', [CartController::class, 'update']);
 
-Route::get('/checkout', [App\Controllers\Front\CheckoutController::class, 'index'], 'requireLogin');
-Route::post('/checkout/process', [App\Controllers\Front\CheckoutController::class, 'process'], 'requireLogin');
-Route::get('/checkout/success', [App\Controllers\Front\CheckoutController::class, 'success'], 'requireLogin');
-Route::get('/checkout/failed', [App\Controllers\Front\CheckoutController::class, 'failed'], 'requireLogin');
+Route::get('/checkout', [CheckoutController::class, 'index'], 'requireLogin');
+Route::post('/checkout/process', [CheckoutController::class, 'process'], 'requireLogin');
+Route::get('/checkout/success', [CheckoutController::class, 'success'], 'requireLogin');
+Route::get('/checkout/failed', [CheckoutController::class, 'failed'], 'requireLogin');
 
 Route::get('/admin', [App\Controllers\Admin\DashboardController::class, 'index'], 'requireAdmin');
-Route::get('/admin/products', [App\Controllers\Admin\DashboardController::class, 'products'], 'requireAdmin');
+Route::get('/admin/products', [App\Controllers\Admin\ProductController::class, 'index'], 'requireAdmin');
 Route::get('/admin/products/create', [App\Controllers\Admin\ProductController::class, 'create'], 'requireAdmin');
+Route::get('/admin/products/api', [App\Controllers\Admin\ProductController::class, 'api'], 'requireAdmin');
+Route::get('/admin/products/([a-z0-9_]+(?:-[a-z0-9]+)*)/edit', [App\Controllers\Admin\ProductController::class, 'edit'], 'requireAdmin');
+Route::post('/admin/products/([a-z0-9_]+(?:-[a-z0-9]+)*)/update', [App\Controllers\Admin\ProductController::class, 'update'], 'requireAdmin');
+Route::get('/admin/products/(\d+)/stocks', [App\Controllers\Admin\ProductController::class, 'stocks'], 'requireAdmin');
+Route::post('/admin/products/store', [App\Controllers\Admin\ProductController::class, 'store'], 'requireAdmin');
 
 Route::get('/customer', [App\Controllers\Customer\DashboardController::class, 'index'], 'requireLogin');
 Route::get('/customer/invoice', [App\Controllers\Customer\DashboardController::class, 'invoice'], 'requireLogin');
 Route::post('/customer/redeem', [App\Controllers\Customer\DashboardController::class, 'redeemProduct'], 'requireLogin');
 
-Route::get('/auth/login', [App\Controllers\Auth\AuthController::class, 'login'], 'redirectIfLoggedIn');
-Route::get('/auth/register', [App\Controllers\Auth\AuthController::class, 'register'], 'redirectIfLoggedIn');
-Route::post('/auth/authenticate', [App\Controllers\Auth\AuthController::class, 'authenticate'], 'redirectIfLoggedIn');
-Route::post('/auth/registration', [App\Controllers\Auth\AuthController::class, 'registration'], 'redirectIfLoggedIn');
-Route::post('/auth/logout', [App\Controllers\Auth\AuthController::class, 'logout'], 'requireLogin');
+Route::get('/auth/login', [AuthController::class, 'login'], 'redirectIfLoggedIn');
+Route::get('/auth/register', [AuthController::class, 'register'], 'redirectIfLoggedIn');
+Route::post('/auth/authenticate', [AuthController::class, 'authenticate'], 'redirectIfLoggedIn');
+Route::post('/auth/registration', [AuthController::class, 'registration'], 'redirectIfLoggedIn');
+Route::post('/auth/logout', [AuthController::class, 'logout'], 'requireLogin');
 
 Route::get('/products', [App\Controllers\Front\ProductController::class, 'index']);
 Route::get('/products/search', [App\Controllers\Front\ProductController::class, 'search']);
 Route::get('/products/([a-z0-9_]+(?:-[a-z0-9]+)*)', [App\Controllers\Front\ProductController::class, 'show']);
+
+Route::get('/admin/product/codes/generate', function () {
+    echo '<pre>';
+    for ($i=0; $i < 10; $i++) {
+        var_dump((new \App\Models\ProductCode)->create([
+            'product_id' => rand(1, 10),
+            'user_id' => null,
+            'status' => 0,
+            'activation_code' => generateActivationCode()
+        ]));
+    }
+    echo '</pre>';
+}, 'requireAdmin');
--- App/views/admin/products/edit.php
+++ App/views/admin/products/edit.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
 <?php importView('sections.dashboard.head', ['css' => [
+  'modules/datatables/datatables.min.css',
+  'modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css',
   'modules/summernote/summernote-bs4.css',
   'modules/jquery-selectric/selectric.css',
   'modules/bootstrap-daterangepicker/daterangepicker.css',
@@ -22,36 +24,37 @@
             <div class="section-header-back">
               <a href="<?= route('admin/products') ?>" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
             </div>
-            <h1>Create New Product</h1>
+            <h1>Edit Product</h1>
             <div class="section-header-breadcrumb">
               <div class="breadcrumb-item active"><a href="<?= route('admin') ?>">Dashboard</a></div>
               <div class="breadcrumb-item"><a href="<?= route('admin/products') ?>">Products</a></div>
-              <div class="breadcrumb-item">Create New Product</div>
+              <div class="breadcrumb-item">Edit Product</div>
             </div>
           </div>
 
           <div class="section-body">
-            <h2 class="section-title">Create New Product</h2>
+            <h2 class="section-title">Edit Product</h2>
 
             <div class="row">
               <div class="col-12">
                 <?php importView('sections.dashboard.validation-alert'); ?>
-                <div class="card">
+                <div class="card card-primary">
                   <div class="card-header">
                     <h4>Product Information</h4>
                   </div>
                   <div class="card-body">
-                    <form action="<?= route('admin/products/store') ?>" method="POST">
+                    <form action="<?= route('admin/products/'. $product->slug .'/update') ?>" method="POST">
+                      <input type="hidden" name="product_id" value="<?= $product->id ?>">
                       <div class="form-group row mb-4">
                         <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Title</label>
                         <div class="col-sm-12 col-md-7">
-                          <input type="text" class="form-control" name="title" value="<?= old('title') ?>" >
+                          <input type="text" class="form-control" name="title" value="<?= old('title', $product->title) ?>" >
                         </div>
                       </div>
                       <div class="form-group row mb-4">
                         <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Code</label>
                         <div class="col-sm-12 col-md-7">
-                          <input type="text" class="form-control" name="code" value="<?= old('code') ?>" required>
+                          <input type="text" class="form-control" name="code" value="<?= old('code', $product->code) ?>" required>
                         </div>
                       </div>
                       <div class="form-group row mb-4">
@@ -62,7 +65,7 @@
                               Rp
                             </div>
                           </div>
-                          <input type="text" class="form-control currency" name="price" value="<?= old('price') ?>" required>
+                          <input type="text" class="form-control currency" name="price" value="<?= old('price', $product->price) ?>" required>
                         </div>
                       </div>
                       <div class="form-group row mb-4">
@@ -70,7 +73,7 @@
                         <div class="col-sm-12 col-md-7">
                           <select class="form-control selectric" name="developer">
                           <?php foreach($developers as $developer): ?>
-                            <option value="<?= $developer->id ?>"><?= $developer->name ?></option>
+                            <option value="<?= $developer->id ?>" <?= $product->developer_id == $developer->id ? 'selected' : '' ?> ><?= $developer->name ?></option>
                           <?php endforeach; ?>
                           </select>
                         </div>
@@ -80,7 +83,7 @@
                         <div class="col-sm-12 col-md-7">
                           <select class="form-control selectric" name="category">
                           <?php foreach($categories as $category): ?>
-                            <option value="<?= $category->id ?>"><?= $category->name ?></option>
+                            <option value="<?= $category->id ?>" <?= $product->category_id == $category->id ? 'selected' : '' ?>><?= $category->name ?></option>
                           <?php endforeach; ?>
                           </select>
                         </div>
@@ -88,40 +91,57 @@
                       <div class="form-group row mb-4">
                         <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Short Description</label>
                         <div class="col-sm-12 col-md-7">
-                          <textarea class="summernote-simple" name="short_description"><?= old('short_description') ?></textarea>
+                          <textarea class="summernote-simple" name="short_description"><?= old('short_description', $product->short_description) ?></textarea>
                         </div>
                       </div>
                       <div class="form-group row mb-4">
                         <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Long Description</label>
                         <div class="col-sm-12 col-md-7">
-                          <textarea class="summernote-simple" name="description"><?= old('description') ?></textarea>
+                          <textarea class="summernote-simple" name="description"><?= old('description', $product->description) ?></textarea>
                         </div>
                       </div>
                       <div class="form-group row mb-4">
                         <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Image</label>
                         <div class="col-sm-12 col-md-7">
-                          <input type="text" class="form-control" name="image" value="<?= old('image') ?>" placeholder="Link image" required>
+                          <input type="text" class="form-control" name="image" value="<?= old('image', $product->image) ?>" placeholder="Link image" required>
                         </div>
                       </div>
                       <div class="form-group row mb-4">
-                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Released At</label>
+                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Released Date</label>
                         <div class="col-sm-12 col-md-7">
-                          <input type="text" name="released_at" class="form-control datepicker" value="<?= old('released_at') ?>">
-                        </div>
-                      </div>
-                      <div class="form-group row mb-4">
-                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Activation Codes</label>
-                        <div class="col-sm-12 col-md-7">
-                          <textarea class="form-control" style="min-height: 150px" name="game_codes" required><?php for($i = 0; $i <= 10; $i++) echo generateActivationCode() . PHP_EOL; ?></textarea>
+                          <input type="text" name="released_at" class="form-control datepicker" value="<?= old('released_at', dt($product->released_at, 'j F Y', 'Y-m-d')) ?>">
                         </div>
                       </div>
                       <div class="form-group row mb-4">
                         <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
                         <div class="col-sm-12 col-md-7">
-                          <button class="btn btn-primary">Add Product</button>
+                          <button class="btn btn-primary">Save Product</button>
                         </div>
                       </div>
                     </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="card card-info">
+                  <div class="card-header">
+                    <h4>Product Stocks</h4>
+                  </div>
+                  <div class="card-body">
+                    <div class="table-responsive">
+                      <table class="table table-striped" id="table-stocks">
+                        <thead>
+                          <tr>
+                            <td>ID</td>
+                            <td>Activation Code</td>
+                            <td>Created At</td>
+                            <td>Updated At</td>
+                          </tr>
+                        </thead>
+                      </table>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -140,10 +160,13 @@
     'modules/jquery-selectric/jquery.selectric.min.js',
     'modules/upload-preview/assets/js/jquery.uploadPreview.min.js',
     'modules/bootstrap-daterangepicker/daterangepicker.js',
-    'modules/cleave-js/dist/cleave.min.js'
+    'modules/cleave-js/dist/cleave.min.js',
+    'modules/datatables/datatables.min.js',
+    'modules/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js',
   ]]) ?>
 
   <script>
+  var editor;
   $(document).ready(function() {
     $("select").selectric();
     $.uploadPreview({
@@ -155,10 +178,22 @@
       no_label: false,                // Default: false
       success_callback: null          // Default: null
     });
-  });
-  var cleaveC = new Cleave('.currency', {
-    numeral: true,
-    numeralThousandsGroupStyle: 'thousand'
+    var cleaveC = new Cleave('.currency', {
+      numeral: true,
+      numeralThousandsGroupStyle: 'thousand'
+    });
+
+    $('#table-stocks').DataTable({
+      ajax: '<?= route('admin/products/'. $product->id .'/stocks') ?>',
+      ordering: false,
+      columns: [
+        { data: "id" },
+        { data: "activation_code" },
+        { data: "created_at" },
+        { data: "updated_at" }
+      ]
+    });
+
   });
   </script>
 
