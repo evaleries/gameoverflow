c2d8d7ed9f36a8ca21bc88014ab8d6a85860470c
evaleries <didaarda15@gmail.com>
2020-08-08 05:57:41 +0700

    Encode meta tags

--- App/views/sections/front/head.php
+++ App/views/sections/front/head.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="<?= isset($pageMeta['description']) ? $pageMeta['description'] : 'Gameoverflow best selling games platform' ?>">
-    <meta name="keywords" content="<?= isset($pageMeta['keywords']) ? implode(', ', $pageMeta['keywords']) : 'gameoverflow, game' ?>">
+    <meta name="description" content="<?= isset($pageMeta['description']) ? __e($pageMeta['description']) : 'Gameoverflow best selling games platform' ?>">
+    <meta name="keywords" content="<?= isset($pageMeta['keywords']) ? __e(implode(', ', $pageMeta['keywords'])) : 'gameoverflow, game' ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php if (isset($pageTitle)) { ?>
