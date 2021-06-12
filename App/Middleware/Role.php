d094db50fc2ec2e2d46e005af88cf12a0a845ee3
evaleries <48923153+evaleries@users.noreply.github.com>
2021-06-13 02:56:54 +0700

    use authmanager

--- App/Middleware/Role.php
+++ App/Middleware/Role.php
@@ -12,22 +12,22 @@ class Role extends Middleware
 {
     public function handle(Request $request, Closure $next, $roles = [])
     {
-        if (!session()->has('__auth')) {
+        if (!auth()->check()) {
             return redirect('auth/login');
         }
 
-        if (is_string($roles) && strtolower($roles) === strtolower(auth()->getRoleName())) {
+        if (is_string($roles) && strtolower($roles) === strtolower(auth()->user()->getRoleName())) {
             return $next($request);
         }
 
         if (is_array($roles)) {
             foreach ($roles as $role) {
                 $role = strtolower($role);
-                if ($role === 'admin' && auth()->isAdmin()) {
+                if ($role === 'admin' && auth()->user()->isAdmin()) {
                     return $next($request);
                 }
 
-                if ($role === strtolower(auth()->getRoleName())) {
+                if ($role === strtolower(auth()->user()->getRoleName())) {
                     return $next($request);
                 }
             }
